bab24c1b0a780b33997221dc12aa1dccd6710a6a
luciana-pereira <rm92014@fiap.com.br>
2022-06-26 23:03:46 -0300

    correction of login screen layout and inclusion of icons and footer.

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
 import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import styles from './SignIn.module.css';
 import Input from '../Forms/Input';
-import Copyright from '../Copyright/Copyright';
 import Button from '../Forms/Button';
-
-const theme = createTheme();
+import AppleStore from '../../assets/store.jpg'
+import GoogleStore from '../../assets/google-store.png'
+import Facebook from '../../assets/facebook.png';
 
 export const SignIn = () => {
-
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -24,194 +19,128 @@ export const SignIn = () => {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
+    <div className={styles.signInContainer}>
+      <Box className={styles.signInContent}>
         <Box
-          sx={{
-            color: 'rgba(var(--i1d,38,38,38),1)',
-            marginTop: '12px',
-            maxWidth: '350px',
-            flexGrow: 1,
-          }}
+          component="form"
+          onSubmit={handleSubmit}
+          noValidate
+          className={styles.form}
         >
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{
-              alignItems: 'center',
-              backgroundColor: 'rgba(var(--d87,255,255,255),1)',
-              border: '1px solid rgba(var(--b6a,219,219,219),1)',
-              borderRadius: '1px',
-              margin: '44px 0 10px',
-              padding: ' 10px 0',
-            }}
-          >
-            <Box
-              sx={{
-                display: 'flex',
-                alignContent: 'stretch',
-                alignItems: 'stretch',
-                justifyContent: 'flex-start',
-                flexDirection: 'column',
-                flex: '0 0 auto',
-                marginBottom: '12px',
-                marginTop: '36px',
-                cursor: 'pointer',
-              }}
-            >
-              <h1 className={styles.title}>Instagram</h1>
-            </Box>
+          <Box className={styles.logoContainer}>
+            <img
+              alt={'logo Instagram'}
+              className={styles.logo}
+              src={
+                '	https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png'
+              }
+            />
+          </Box>
 
-            <Box
-              sx={{
-                marginBottom: '10px',
-                // maxWidth: '350px',
-                width: '100%',
-              }}
-            >
-              <form
-                id="loginForm"
-                method="post"
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                }}
-              >
-                <Box
-                  sx={{
-                    display: 'flex',
-                    alignContent: 'stretch',
-                    alignItems: 'stretch',
-                    justifyContent: 'flex-start',
-                    flexDirection: 'column',
-                    flex: '0 0 auto',
-                  }}
-                >
-                  <Box
-                    sx={{
-                      margin: '0 40px 6px',
-                    }}
-                  >
-                    <Box
-                      sx={{
-                        alignItems: 'center',
-                        borderRadius: '3px',
-                        boxSizing: 'border-box',
-                        display: 'flex',
-                        flexDirection: 'row',
-                        fontSize: '14px',
-                        position: 'relative',
-                        width: '100%',
-                      }}
-                    >
-                      <Input
-                        placeholder={'Telefone, nome de usuário ou email'}
-                        type={'text'} name={'username'}
-                        value={undefined}
-                        onChange={() => {}}
-                        error={'Campo obrigatório'}
-                        onBlur={() => {}} />
-                      {/* <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="username"
-                        label="Telefone, nome de usuário ou email"
-                        name="username"
-                        autoComplete="username"
-                        autoFocus
-                      /> */}
-                    </Box>
-
-                    <Box
-                      sx={{
-                        alignItems: 'center',
-                        borderRadius: '3px',
-                        boxSizing: 'border-box',
-                        display: 'flex',
-                        flexDirection: 'row',
-                        fontSize: '14px',
-                        position: 'relative',
-                        width: '100%',
-                      }}
-                    >
-                      <Input
-                        type={'password'}
-                        name={'password'}
-                        value={undefined}
-                        onChange={() => {}}
-                        error={'Campo obrigatório'}
-                        onBlur={() => {}}
-                        placeholder={'Senha*'}
-                      />
-                      {/* <TextField
-                        margin="normal"
-                        required
-                        name="password"
-                        label="Senha"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                      /> */}
-                    </Box>
-                    <Button children={'Entrar'} />
-                    {/* <Button
-                      type="submit"
-                      fullWidth
-                      variant="contained"
-                      sx={{ mt: 3, mb: 2 }}
-                    >
-                      Entrar
-                    </Button> */}
-                  </Box>
+          <Box className={styles.formContainer}>
+            <form id="loginForm" method="post" className={styles.formContent}>
+              <Box className={styles.content}>
+                <Box className={styles.inputContent}>
+                  <Input
+                    type={'text'}
+                    name={'username'}
+                    value={undefined}
+                    onChange={() => {}}
+                    onBlur={() => {}}
+                    label={'Telefone, nome de usuário ou email'}
+                  />
+                  {/* {error && <p className={styles.error}>Campo obrigatório</p>} */}
                 </Box>
 
-                <Link
-                  href="#"
-                  textAlign="center"
-                  fontSize="12px"
-                >
-                  Esqueceu a senha?
-                </Link>
-              </form>
-            </Box>
-          </Box>
+                <Box className={styles.inputContent}>
+                  <Input
+                    type={'password'}
+                    name={'password'}
+                    value={undefined}
+                    onChange={() => {}}
+                    onBlur={() => {}}
+                    label={'Senha*'}
+                  />
+                </Box>
+                <Button children={'Entrar'} />
+                <Box className={styles.lineContainer}>
+                  <div className={styles.line}></div>
+                  <div className={styles.or}>ou</div>
+                  <div className={styles.line}></div>
+                </Box>
 
-          <Box
-            sx={{
-              alignItems: 'center',
-              backgroundColor: 'rgba(var(--d87,255,255,255),1)',
-              border: '1px solid rgba(var(--b6a,219,219,219),1)',
-              borderRadius: '1px',
-              margin: '0 0 10px',
-              padding: '30px 0',
-            }}
-          >
-            <Typography
-              style={{
-                fontSize: "14px",
-                textAlign: "center"
-              }}
-            >
-              Não tem uma conta?
-            </Typography>
-            <Link
-              href="#"
-              style={{
-                fontSize: "14px",
-                textAlign: "center",
-                fontWeight: "bold",
-                textDecoration: "none",
-                color: "#0095f6",
-              }}
-            >
-              Cadastre-se
-            </Link>
+                <Box className={styles.facebookContainer}>
+                  <div className={styles.facebookContent}>
+                    <span className={styles.iconContent}>
+                      <img
+                        className={styles.icon}
+                        src={Facebook}
+                        alt="icone facebook"
+                      />
+                    </span>
+                    <span className={styles.text}>Entrar com o Facebook</span>
+                  </div>
+                </Box>
+              </Box>
+
+              <Link
+                href="#"
+                sx={{
+                  color: '#00376b',
+                  fontSize: '10px',
+                  lineHeight: '16px',
+                  marginTop: '12px',
+                  textDecoration: 'none',
+                  textAlign: 'center',
+                }}
+              >
+                Esqueceu a senha?
+              </Link>
+            </form>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
+
+        <Box
+          className={styles.registerContainer}
+        >
+          <Box
+            className={styles.registerContent}
+          >
+            <p
+              className={styles.registerText}
+            >
+              Não tem uma conta?
+              {" "}
+              <a
+                href="#"
+                className={styles.registerLink}
+              >
+                Cadastre-se
+              </a>
+            </p>
+          </Box>
+        </Box>
+
+        <Box>
+          <p className={styles.textApp}>
+            Obtenha o aplicativo
+          </p>
+
+          <div className={styles.logoApp}>
+              <img
+                className={styles.iconApp}
+                src={AppleStore}
+                alt="Baixar na App store"
+              />
+              {" "}
+              <img
+                className={styles.iconApp}
+                src={GoogleStore}
+                alt="Baixar no Google play"
+              />
+          </div>
+        </Box>
+      </Box>
+    </div>
   );
 };
